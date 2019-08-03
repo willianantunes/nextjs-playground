4aef38a6e1da10101cc9291e6faa33104de37817
Willian Antunes <willian.lima.antunes@gmail.com>
2019-08-03 16:30:24 -0300

    `useRouter` being added to a component other than the exported page

--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,20 @@
 import { useRouter } from 'next/router';
-import Layout from '../components/MyLayout';
+import Layout from '../components/MyLayout.js';
 
-const Page = () => {
+const Content = () => {
   const router = useRouter();
-
   return (
-    <Layout>
+    <>
       <h1>{router.query.title}</h1>
       <p>This is the blog post content.</p>
-    </Layout>
+    </>
   );
 };
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+);
 
 export default Page;
