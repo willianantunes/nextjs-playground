efd25955b385306d0c53a45e587d62045682ffa3
Willian Antunes <willian.lima.antunes@gmail.com>
2019-08-03 18:46:21 -0300

    Fetching data for pages through TVMaze API

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Useful links:
 - https://nextjs.org/learn/basics/getting-started
 - https://github.com/zeit/next.js/issues/4789
 - https://github.com/zeit/next.js/pull/936
+- http://www.tvmaze.com/api
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
-  <li>
-    <Link href='/p/[id]' as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href='/p/[id]' as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id='hello-nextjs' />
-        <PostLink id='learn-nextjs' />
-        <PostLink id='deploy-nextjs' />
-      </ul>
-    </Layout>
-  );
-}
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show),
+  };
+};
+
+export default Index;
