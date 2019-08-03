b781e06722a91c9584001cf7d723767338c1a793
Willian Antunes <willian.lima.antunes@gmail.com>
2019-08-03 19:05:27 -0300

    The correct way to apply CSS to a component as nested is not allowed

--- pages/css.js
+++ pages/css.js
@@ -14,6 +14,22 @@ const PostLink = ({ post }) => (
     <Link href='/p/[id]' as={`/p/${post.id}`}>
       <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
