fab57e4eeb78e735e760354c635690d698f71efc
Willian Antunes <willian.lima.antunes@gmail.com>
2019-08-03 18:51:53 -0300

    Another example of getInitialProps
    https://nextjs.org/docs/#fetching-data-and-component-lifecycle

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,22 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-    const router = useRouter();
+const Post = props => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium} />
+    </Layout>
+);
 
-    return (
-        <Layout>
-            <h1>{router.query.id}</h1>
-            <p>This is the blog post content.</p>
-        </Layout>
-    );
-}
+Post.getInitialProps = async function(context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
+};
+
+export default Post;
