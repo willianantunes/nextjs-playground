a3c2e6a0b107d628a1fcb90ff6bef73ea6643fb3
Willian Antunes <willian.lima.antunes@gmail.com>
2019-12-25 13:14:15 -0300

    Custom API to get page details on the server side
    It will be updated to be full async. More details how I got this done:
    - https://dev.to/metamas/testing-next-js-api-routes-55g3
    - http://bit.ly/376G7OB

---
+++ src/pages/api/page-details.js
@@ -0,0 +1,16 @@
+import * as myCustomHttp from '../../infra/Http';
+import { JSDOM } from 'jsdom';
+
+export default async function handler(request, response) {
+  if (request.query.hasOwnProperty('url')) {
+    const urlToGetDetails = request.query.url;
+    const result = await myCustomHttp.get(urlToGetDetails);
+    const rawPage = await result.text();
+    const parsedPageAsDom = new JSDOM(rawPage);
+    const pageTitle = parsedPageAsDom.window.document.querySelector('title').text;
+    response.setHeader('Content-Type', 'application/json');
+    response.json({ title: pageTitle });
+  }
+
+  response.status(400).end();
+}
