62be03e455366220dbb389dea783f10cc7adeb09
Willian Antunes <willian.lima.antunes@gmail.com>
2019-12-26 16:27:36 -0300

    Health check endpoint

---
+++ __tests__/integration/pages/api/health.spec.test.js
@@ -0,0 +1,49 @@
+import dns from 'dns';
+import http from 'http';
+import fetch from 'isomorphic-unfetch';
+import listen from 'test-listen';
+import { apiResolver } from 'next/dist/next-server/server/api-utils';
+import handler from '../../../../src/pages/api/health';
+
+describe('Integrations tests for health endpoint', () => {
+  let server;
+  let url;
+
+  beforeAll(async done => {
+    server = http.createServer((req, res) => apiResolver(req, res, undefined, handler));
+    url = await listen(server);
+    done();
+  });
+
+  afterAll(done => {
+    server.close(done);
+  });
+
+  test('Should return 200 informing internet status if OK', async () => {
+    // dns.promises.lookup.mockReset();
+    let response = await fetch(url);
+    const jsonResult = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(jsonResult).toMatchObject({
+      internetAvailable: true,
+    });
+  });
+
+  test('Should return 503 if internet is not available to be consulted', async () => {
+    const lookupSpy = jest.spyOn(dns.promises, 'lookup').mockImplementation(
+      hostname =>
+        new Promise((resolve, reject) => {
+          reject('Just to force an error on purpose');
+        }),
+    );
+    let response = await fetch(url);
+    const jsonResult = await response.json();
+
+    expect(lookupSpy).toHaveBeenCalledWith('github.com');
+    expect(response.status).toBe(503);
+    expect(jsonResult).toMatchObject({
+      internetAvailable: false,
+    });
+  });
+});
---
+++ src/pages/api/health.js
@@ -0,0 +1,16 @@
+import dns from 'dns';
+
+export default async function handler(request, response) {
+  dns.promises
+    .lookup('github.com')
+    .then(() =>
+      response.json({
+        internetAvailable: true,
+      }),
+    )
+    .catch(() =>
+      response.status(503).json({
+        internetAvailable: false,
+      }),
+    );
+}
