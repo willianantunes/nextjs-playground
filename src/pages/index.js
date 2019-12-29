ebb73bc3f3476b5115cc16c5730ea4dc1287a3ae
Willian Antunes <willian.lima.antunes@gmail.com>
2019-12-29 11:14:38 -0300

    A new page to test the challenge
    It will be separated is various components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,27 @@
 import { Component } from 'react';
 import evaluate from '../business/MessageParser';
+import uuidv4 from 'uuid/v4';
+import Link from 'next/link';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 
 class Index extends Component {
   constructor() {
     super();
-    this.state = {
-      messages: [],
-      configuredMessage:
-        'Hey @Aladdin and @Genie, you wanna help on your (nose)?! Know more accessing https://gist.github.com/willianantunes',
-    };
+    this.state = this.initialState();
   }
+
+  initialState = () => ({
+    messages: [],
+    configuredMessage:
+      'Hey @Aladdin and @Genie, you wanna help on your (nose)?! Know more accessing https://gist.github.com/willianantunes',
+  });
 
   sendForm = async event => {
     event.preventDefault();
     const value = await evaluate(this.state.configuredMessage);
     this.setState({
-      messages: this.state.messages.concat({ original: this.state.configuredMessage, parsed: value })
+      messages: this.state.messages.concat({ id: uuidv4(), original: this.state.configuredMessage, parsed: value }),
+      configuredMessage: '',
     });
   };
 
@@ -23,50 +29,65 @@ class Index extends Component {
     this.setState({ configuredMessage: event.target.value });
   };
 
+  cleanAllMessages = event => {
+    event.preventDefault();
+    this.setState({ messages: [], configuredMessage: '' });
+  };
+
   render = () => {
     return (
-      <div id={'root'}>
+      <div>
         <header>
-          <h1>Next.JS Playground Project</h1>
-          <nav>
-            <a href={'#Home'}>Home</a>
-            <a href={'#Batman'}>Batman shows</a>
+          <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+            <Link href='/'>
+              <a className='navbar-brand'>Next.JS Playground Project</a>
+            </Link>
+            <div className='collapse navbar-collapse' id='navbarCollapse'>
+              <ul className='navbar-nav mr-auto'>
+                <Link href='/'>
+                  <a className='nav-item active nav-link'>
+                    Home <span className='sr-only'>(current)</span>
+                  </a>
+                </Link>
+                <Link href='/batman'>
+                  <a className='nav-item nav-link'>Batman shows</a>
+                </Link>
+              </ul>
+            </div>
           </nav>
         </header>
-        <main>
-          <h1>Challenge answer</h1>
+        <main className='container'>
+          <div className='row justify-content-center'>
+            <h1>Challenge answer</h1>
+          </div>
           <form onSubmit={this.sendForm}>
-            <section>
-              <label htmlFor='chatMessage'>Tell us your story:</label>
+            <div className='form-group'>
+              <label htmlFor='chatMessage'>Type what you want to be translated:</label>
               <textarea
+                className='form-control'
                 id='chatMessage'
-                name='chatMessage'
+                rows='3'
                 value={this.state.configuredMessage}
                 onChange={this.setConfiguredMessage}
-                rows='5'
-                cols='33'
               />
-            </section>
-            <section>
-              <p>
-                <button type='submit'>See it parsed!</button>
-              </p>
-            </section>
+            </div>
+            <button className='btn btn-primary' type='submit'>
+              See it parsed!
+            </button>
+            <button className='btn btn-secondary ml-2' onClick={this.cleanAllMessages}>
+              Clear results
+            </button>
           </form>
-          <section>
-            <ul>
-              {this.state.messages.map(message => {
-                return (
-                  <li>
-                    <article>
-                      <h3>{message.original}</h3>
-                      <p>{JSON.stringify(message.parsed)}</p>
-                    </article>
-                  </li>
-                );
-              })}
-            </ul>
-          </section>
+          <div className='list-group pt-3'>
+            {this.state.messages.map(message => {
+              return (
+                <div className='list-group-item list-group-item-action' key={message.id}>
+                  <p className='mb-1'>{message.original}</p>
+                  <small>{JSON.stringify(message.parsed)}</small>
+                </div>
+              );
+            })}
+          </div>
         </main>
       </div>
     );
