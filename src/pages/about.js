5be9babcdf14db724830522cd3b26e1b1e786ce2
Willian Antunes <willian.lima.antunes@gmail.com>
2019-12-31 09:54:04 -0300

    Unused pages

--- src/pages/about.js
+++
@@ -1,7 +0,0 @@
-import Layout from '../components/MyLayout.js';
-
-const aboutPageContent = <p>This is the about page</p>;
-
-export default function About() {
-  return <Layout content={aboutPageContent} />;
-}
