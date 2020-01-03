1e7d622be344a610c148bae67f0fd0e97f742fe0
Willian Antunes <willian.lima.antunes@gmail.com>
2020-01-03 13:22:29 -0300

    Basic use of Code Climate and its badges

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,10 @@ module.exports = {
   ...defaults,
   moduleFileExtensions: ['js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverage: true,
   coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js', '/.next/'],
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/mocks.js',
