7170e35b66ed17acc0cfa05e0f6404a9b278954a
Willian Antunes <willian.lima.antunes@gmail.com>
2020-01-04 19:29:56 -0300

    Logging with logplease. It works in BE and FE
    Useful links:
    - https://github.com/haadcode/logplease
    - http://bit.ly/2rTif24
    - https://github.com/zeit/next.js/issues/1852
    - https://github.com/zeit/next.js/issues/4808
    - https://github.com/winstonjs/winston/issues/287
    - https://github.com/luffyZh/next-antd-scaffold

--- src/services/PageDetailsService.js
+++ src/services/PageDetailsService.js
@@ -1,8 +1,15 @@
 import * as httpService from '../infra/Http';
+import Logger from '../infra/logger';
+
+const logger = Logger('PageDetailsService');
 
 const endpoint = process.env.PAGE_DETAILS_ENDPOINT;
 
 export async function getDetails(url) {
+  logger.debug(`Details for ${url}`);
   const params = { url };
-  return httpService.get(endpoint, params).then(res => res.json());
+  return httpService.get(endpoint, params).then(res => {
+    logger.debug('Parsing response as JSON...');
+    return res.json();
+  });
 }
